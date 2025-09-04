623f44e5a3563c3212c5860ac1b5a9cd9d86bcff
kennywestt <dlrudrms0617@naver.com>
2025-09-04 23:47:39 +0900

    footer 사이트 기술내용

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,9 @@ export default function Footer() {
         {/* <StarsBackground/> */}
         <div className="max-w-screen-xl mx-auto px-6 text-center text-gray-100">
           © {new Date().getFullYear()} Gyeonggeun. All rights reserved.
+          <p className="mt-2">
+          이 사이트는 React, Tailwindcss, Next.js, three.js 를 사용하여 만들었습니다.
+        </p>
         </div>
       </footer>
     )
